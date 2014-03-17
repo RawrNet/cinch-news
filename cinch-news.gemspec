03862a0edd41bfef553ed15935ef681ab81363a9
Namasteh <risardyo@sinsira.net>
2014-03-17 02:46:38 -0500

    Changed API and rewrote plugin, top down.

--- cinch-news.gemspec
+++ cinch-news.gemspec
@@ -1,13 +1,13 @@
 Gem::Specification.new do |s|
   s.name = "cinch-news"
-  s.version = '1.0.2'
+  s.version = '2.0'
   s.authors = ["Richard Banks"]
   s.email = ["namaste@rawrnet.net"]
   s.license = 'MIT'
   s.homepage = "https://github.com/RawrNet/cinch-news"
   s.summary = "A news gem/plugin for the Cinch IRC bot framework."
   s.description = "A news gem/plugin for the Cinch IRC bot framework " +
-    "that uses a USA Today API. " +     
+    "that uses a FeedZilla API. " +     
     "You can visit irc://rawr.sinsira.net #Lobby to get help, report issues " +
     ", test the gem, or just chat."
 
--- lib/cinch/plugins/news.rb
+++ lib/cinch/plugins/news.rb
@@ -8,82 +8,90 @@ require 'open-uri'
 require 'json'
 require 'cgi'
 require 'cinch/toolbox'
+require_relative "helpers/category_helper.rb"
 
 module Cinch
   module Plugins
     class News
       include Cinch::Plugin
       
+      set :plugin_name, 'news'
+      set :help, <<-USAGE.gsub(/^ {6}/, '')
+        This plugin is a comprehensive news aggregator and contacts several sources for articles.
+        Enable/Disable Usage:
+        * !n-categories: This command returns all the categories that you can choose from in a PM.
+        * !n-subcategories <category ID>: This command returns all the subcategories of the specified category and their useable ID.
+        * !news: This command retrieves the top three articles from the Top Stories subcategory of the Top News category.
+        * !news <category ID>: This command retrieves the top three articles from the specified category.
+        * !news <category ID> <subcategory ID>: This command retrieves the top three articles from the specified subcategory of the specified category.
+        * !s-category <category id> <query>: This command searches the specified category for your query.
+        USAGE
+      
+      match /n-categories/, method: :list_categories
+      
+      match /n-subcategories (.+)/, method: :list_subs
+      
       match /news$/, method: :top_news
+
+      match /news (.+)$/, method: :c_topnews
+
+      match /news (.+?) (.+)/, method: :sc_topnews
       
-      match /news (tech|money|travel|sports|life|weather|nation|offbeat|washington|world|religion|opinion|health|nfl|mlb|nba|nhl|collegefootball|collegebasketball|highschool|motorsports|golf|soccer|olympics|horseracing|people|books|music|movies)/, method: :category_news
+      match /s-category (.+?) (.+)/, method: :search_category
+
+      match /s-subcategory (.+?) (.+?) (.+)/, method: :search_subcategory
       
-      match /s-news (.+)/, method: :search_news
+      def list_categories(m)
+        cat_list = categories
+        ca_list = cat_list.join(", ")
+        m.user.notice "#{ca_list}"
+      end
       
+      def list_subs(m, c_id)
+        sc_list = sub_categories(c_id)
+        s_list = sc_list.join(", ")
+        m.user.notice "#{s_list}"
+      end
+    
+      
+      def search_category(m, id, terms)
+        terms.gsub! /\s/, '+'
+        search_list = execute_c_search(id, terms)
+        search_list[0..2].each{|i| m.reply i}
+          rescue OpenURI::HTTPError
+            m.reply "Error: #{$!}"
+          end
+
+      def search_subcategory(m, id, sc_id, terms)
+        terms.gsub! /\s/, '+'
+        sc_search_list = execute_sc_search(id, sc_id, terms)
+        sc_search_list[0..2].each{|i| m.reply i}
+          rescue OpenURI::HTTPError
+            m.reply "Error #{$!}"
+          end 
+
       def top_news(m)
-        data = fetch_news(m)
-        return m.reply "There was a problem fetching the news." if data.empty?
-        output_news(m, data)
-      end
-      
-      def category_news(m, category)
-        data = fetch_category(m, category)
-        return m.reply "There was a problem fetching the news for #{category}." if data.empty?
-        output_news(m, data)
-      end
-      
-      def search_news(m, terms)
-        data = fetch_search(m, terms)
-        return m.reply "No news results found for #{terms}." if data == false
-        output_news(m, data)
-      end
-      
-      def fetch_news(m)
-        key = config[:key]
-        data = JSON.parse(open("http://api.usatoday.com/open/articles/topnews?api_key=#{key}&encoding=json&count=3").read)
-        stories = data['stories']
-        output = []
-        stories.each{|i|
-        url = i['link']
-        url.gsub! /apidata./, ''
-        url = Cinch::Toolbox.shorten(url)
-        output.push("News: | \"%s\" | Description: %s [ #{url} ]" % [i['title'], i['description']])}
-        return output
-      end
-      
-      def fetch_category(m, category)
-        key = config[:key]
-        data = JSON.parse(open("http://api.usatoday.com/open/articles/topnews/#{category}?api_key=#{key}&encoding=json&count=3").read)
-        stories = data['stories']
-        output = []
-        stories.each{|i|
-        url = i['link']
-        url.gsub! /apidata./, ''
-        url = Cinch::Toolbox.shorten(url)
-        output.push("News: | \"%s\" | Description: %s [ #{url} ]" % [i['title'], i['description']])}
-        return output
-      end
-      
-      def fetch_search(m, terms)
-        key = config[:key]
-        search_uri = URI.encode("http://api.usatoday.com/open/articles?tag=#{terms}&count=3&api_key=#{key}&encoding=json")
-        data = JSON.parse(open("#{search_uri}").read)
-        stories = data['stories']
-        output = []
-        stories.each{|i|
-        url = i['link']
-        url.gsub! /apidata./, ''
-        url = Cinch::Toolbox.shorten(url)
-        output.push("News: | \"%s\" | Description: %s [ #{url} ]" % [i['title'], i['description']])}
-        return output
-        rescue JSON::ParserError
-          return false
-      end
-      
-      def output_news(m, data)
-        return if data == false
-        data[0..2].each{|i| m.reply i}
-      end
-    end
+        news = fetch_news
+        news[0..2].each{|i| m.reply i}
+          rescue OpenURI::HTTPError
+            m.reply "Error #{$!}"
+          end
+
+      def c_topnews(m, id)
+        c_news = c_top_news(id)
+        c_news[0..2].each{|i| m.reply i}
+          rescue OpenURI::HTTPError
+            m.reply "Error #{$!}"
+          end
+
+      def sc_topnews(m, id, sc_id)
+        sc_news = sc_top_news(id, sc_id)
+        sc_news[0..2].each{|i| m.reply i}
+          rescue OpenURI::HTTPError
+            m.reply "Error #{$!}"
+          end
+
+
+        end
   end
 end
